19cec2c60732ec2586a0e480798696c0493b93a4
HeYue <879424608@qq.com>
2019-09-13 16:46:50 +0800

    ajax登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -25,6 +25,9 @@ $(function () {
                         min: 2,
                         max: 6,
                         message: '用户名长度必须在2到6位之间'
+                    },
+                    callback: {
+                        message: '用户名不存在'
                     }
                 }
             },
@@ -39,10 +42,43 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: '密码长度必须在6到12位之间'
+                    },
+                    callback: {
+                        message: '密码错误'
                     }
                 }
             }
         }
 
     });
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function(info) {
+                if (info.success) {
+                    location.href = 'index.html';
+                } 
+                if (info.error === 1000) {
+                    // alert('用户名不存在');
+                    $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
+                }
+                if (info.error === 1001) {
+                    // alert('密码错误');
+                    $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+                }
+            }
+        })
+    });
+    $('[type="reset"]').click(function() {
+        // 调用插件方法，进行重置校验状态
+        // resetForm(Boolean);
+        // 传true,重置内容以及校验状态
+        // 传false，只重置校验状态
+        $('#form').data('bootstrapValidator').resetForm();
+    })
 })
